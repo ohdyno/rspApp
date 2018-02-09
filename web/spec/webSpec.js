5e49bb67b4c7e5a0879a6df2907b5c32846b8038
Murtuza Boxwala <mboxwala@pivotal.io>
2018-02-09 17:59:13 -0500

    Pass valid use case tests

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -6,20 +6,47 @@ class RPSApp extends React.Component {
         super(props);
 
         this.state = {
-            useCases: props.userCases
-        }
+            useCases: props.useCases,
+            result: 'INVALID'
+        };
+    }
+
+    clickPlay() {
+        this.state.useCases.play(null, null, this);
+    }
+
+    p1Wins() {
+        this.setState(() => ({
+            result: 'P1 Wins'
+        }));
+    }
+
+    p2Wins() {
+        this.setState(() => ({
+            result: 'P2 Wins'
+        }));
+    }
+
+    invalid() {
+
+    }
+
+    tie() {
+        this.setState(() => ({
+            result: 'Tie'
+        }));
     }
 
     render() {
-        return <button id={'playButton'}>INVALID</button>
+        return <button id={'playButton'} onClick={() => this.clickPlay()}>{this.state.result}</button>
     }
 }
 
 describe("play form", function () {
+    afterEach(() => {
+        tearDown()
+    });
     describe("when the play use case tells the UI that the input is invalid", function () {
-        afterEach(() => {
-            tearDown()
-        });
 
         it('tells the user that their input is invalid', function () {
             renderApp({
@@ -33,6 +60,48 @@ describe("play form", function () {
             expect(pageContent()).toContain("INVALID")
         });
     });
+
+    describe('when the play use case tells the UI that p1 wins', function () {
+        it('tells the user that p1 wins', function () {
+            renderApp({
+                play: function (p1, p2, ui) {
+                    ui.p1Wins()
+                }
+            });
+
+            play();
+
+            expect(pageContent()).toContain("P1 Wins")
+        })
+    });
+
+    describe('when the play use case tells the UI that p2 wins', function () {
+        it('tells the user that p2 wins', function () {
+            renderApp({
+                play: function (p1, p2, ui) {
+                    ui.p2Wins()
+                }
+            });
+
+            play();
+
+            expect(pageContent()).toContain("P2 Wins")
+        })
+    })
+
+    describe('when the play use case tells the UI that p1 and p2 tie', function () {
+        it('tells the user that the p1 and p2 tie', function () {
+            renderApp({
+                play: function (p1, p2, ui) {
+                    ui.tie()
+                }
+            });
+
+            play();
+
+            expect(pageContent()).toContain("Tie")
+        })
+    })
 });
 
 let domFixture;
