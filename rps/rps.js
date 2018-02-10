12d2dc8624d09fbd3bf8973eb0871b19e1c683d2
Xing Zhou <xzhou@pivotal.io>
2018-02-10 06:03:56 -0500

    Pass the invalid use case test for the domain

--- rps/rps.js
+++ rps/rps.js
@@ -1,17 +1,36 @@
 class Requests {
     play(p1, p2, ui) {
-        if (p1 === p2) {
+        if (this.invalid(p1, p2)) {
+            ui.invalid();
+            return
+        }
+        if (this.tie(p1, p2)) {
             ui.tie();
             return
         }
-
-        if (p1 === 'scissors' && p2 === 'paper' ||
-            p1 === 'rock' && p2 === 'scissors' ||
-            p1 === 'paper' && p2 === 'rock') {
+        if (this.p1Wins(p1, p2)) {
             ui.p1Wins();
             return
         }
         ui.p2Wins()
+    }
+
+    p1Wins(p1, p2) {
+        return p1 === 'scissors' && p2 === 'paper' ||
+            p1 === 'rock' && p2 === 'scissors' ||
+            p1 === 'paper' && p2 === 'rock';
+    }
+
+    tie(p1, p2) {
+        return p1 === p2;
+    }
+
+    invalid(p1, p2) {
+        return this.throwIsInvalid(p1) || this.throwIsInvalid(p2);
+    }
+
+    throwIsInvalid(play) {
+        return play !== 'scissors' && play !== 'paper' && play !== 'rock';
     }
 }
 
--- rps/spec/rpsSpec.js
+++ rps/spec/rpsSpec.js
@@ -1,35 +1,35 @@
 const Requests = require('../rps');
 
-describe('play', () => {
+describe('play', function () {
     const requests = new Requests();
     let uiSpy;
 
-    describe('with valid inputs', () => {
-        describe('showing p1 wins', () => {
-            beforeEach(() => {
+    describe('with valid inputs', function () {
+        describe('showing p1 wins', function () {
+            beforeEach(function () {
                 uiSpy = {
                     p1Wins: jasmine.createSpy('p1WinsSpy')
                 }
             });
 
-            it('should show with rock v scissors', () => {
+            it('should show with rock v scissors', function () {
                 requests.play('rock', 'scissors', uiSpy);
                 expect(uiSpy.p1Wins).toHaveBeenCalled()
             });
 
-            it('should show with paper v rock', () => {
+            it('should show with paper v rock', function () {
                 requests.play('paper', 'rock', uiSpy);
                 expect(uiSpy.p1Wins).toHaveBeenCalled()
             });
 
-            it('should show with scissors v paper', () => {
+            it('should show with scissors v paper', function () {
                 requests.play('scissors', 'paper', uiSpy);
                 expect(uiSpy.p1Wins).toHaveBeenCalled()
             });
         });
 
-        describe('showing p2 wins', () => {
-            beforeEach(() => {
+        describe('showing p2 wins', function () {
+            beforeEach(function () {
                 uiSpy = {
                     p2Wins: jasmine.createSpy('p2WinsSpy')
                 }
@@ -40,19 +40,19 @@ describe('play', () => {
                 expect(uiSpy.p2Wins).toHaveBeenCalled()
             });
 
-            it('should show with paper v scissors', () => {
+            it('should show with paper v scissors', function () {
                 requests.play('paper', 'scissors', uiSpy);
                 expect(uiSpy.p2Wins).toHaveBeenCalled()
             });
 
-            it('should show with rock v paper', () => {
+            it('should show with rock v paper', function () {
                 requests.play('rock', 'paper', uiSpy);
                 expect(uiSpy.p2Wins).toHaveBeenCalled()
             });
         });
 
-        describe('showing tie', () => {
-            beforeEach(() => {
+        describe('showing tie', function () {
+            beforeEach(function () {
                 uiSpy = {
                     tie: jasmine.createSpy('tieSpy')
                 }
@@ -71,5 +71,33 @@ describe('play', () => {
                 expect(uiSpy.tie).toHaveBeenCalled()
             });
         })
-    })
+    });
+
+    describe("show invalid result", function () {
+        beforeEach(function () {
+            uiSpy = {
+                invalid: jasmine.createSpy('invalidSpy')
+            }
+        });
+
+        describe("when player 1 gives invalid input", function () {
+            beforeEach(function () {
+                requests.play('sailboat', 'scissors', uiSpy);
+            });
+
+            it('should notify the ui that the result is invalid', function () {
+                expect(uiSpy.invalid).toHaveBeenCalled()
+            });
+        });
+
+        describe("when player 2 gives invalid input", function () {
+            beforeEach(function () {
+                requests.play('scissors', 'sailboat', uiSpy);
+            });
+
+            it('should notify the ui that the result is invalid', function () {
+                expect(uiSpy.invalid).toHaveBeenCalled()
+            });
+        });
+    });
 });
